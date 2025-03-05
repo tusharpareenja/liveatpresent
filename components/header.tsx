6cb688f704d07f5bbb2dbf33dc19c7cc36fe7e03
soravang81 <soravang81@gmail.com>
2025-03-05 13:11:04 +0530

    backup

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex items-center justify-center">
+            <h1 className="text-2xl font-bold">Header</h1>
+        </header>
+    );
+}
